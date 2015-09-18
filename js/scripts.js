dc6fdf8f64f16a27be2c178dadb2d3e18cc3e494
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-09-18 11:24:12 -0700

    add spec for addTopping method

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 //JavaScript
 
+//Topping object:
+function Topping(toppingName, price) {
+  this.toppingName = toppingName;
+  this.price = price;
+}
+
 //PizzaOrder object:
-function PizzaOrder(quantity, toppings, pizzaSize) {
+function PizzaOrder(quantity, pizzaSize) {
   this.quantity = quantity;
   this.pizzaSize = pizzaSize;
   this.toppings = [];
 }
 
-//Topping object:
-function Topping(toppingName, price) {
-  this.toppingName = toppingName;
-  this.price = price;
+PizzaOrder.prototype.addTopping = function() {
+  var topping = new Topping;
+  this.toppings.push(topping);
+}
+
+PizzaOrder.prototype.calculatePrice = function() {
+  var sizePrices = [10, 15, 20];
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,49 @@
-describe('PizzaOrder', function() {
-
-  it("creates an order for a single pizza with no toppings", function() {
-    var testPizzaOrder = new PizzaOrder(1,[],"small");
-    expect(testPizzaOrder.quantity).to.equal(1);
-    expect(testPizzaOrder.pizzaSize).to.eql("small");
-    expect(testPizzaOrder.toppings).to.eql([]);
-  });
-});
-
 describe('Topping', function() {
 
   it("creates a topping with a name and price", function() {
     var testTopping = new Topping("cheese",1);
     expect(testTopping.toppingName).to.equal("cheese");
     expect(testTopping.price).to.equal(1);
+  });
+});
+
+describe('PizzaOrder', function() {
+
+  it("creates an order for a single pizza with no toppings", function() {
+    var testPizzaOrder = new PizzaOrder(1,"small");
+    expect(testPizzaOrder.quantity).to.equal(1);
+    expect(testPizzaOrder.pizzaSize).to.equal("small");
+    expect(testPizzaOrder.toppings).to.eql([]);
+  });
+
+  it("creates an order for a single pizza with one topping", function() {
+    var testPizzaOrder = new PizzaOrder(1,"small");
+    var testTopping = new Topping("cheese",1);
+    testPizzaOrder.addTopping(testTopping);
+    expect(testPizzaOrder.quantity).to.equal(1);
+    expect(testPizzaOrder.pizzaSize).to.equal("small");
+    expect(testPizzaOrder.toppings.length).to.equal(1);
+  });
+
+  it("creates an order for a single pizza with multiple toppings", function() {
+    var testPizzaOrder = new PizzaOrder(1,"small");
+    var testTopping = new Topping("cheese",1);
+    var testTopping2 = new Topping("pepperoni",2);
+    testPizzaOrder.addTopping(testTopping);
+    testPizzaOrder.addTopping(testTopping2);
+    expect(testPizzaOrder.quantity).to.equal(1);
+    expect(testPizzaOrder.pizzaSize).to.equal("small");
+    expect(testPizzaOrder.toppings.length).to.equal(2);
+  });
+
+  it("creates an order for multiple pizzas with multiple toppings", function() {
+    var testPizzaOrder = new PizzaOrder(2,"small");
+    var testTopping = new Topping("cheese",1);
+    var testTopping2 = new Topping("pepperoni",2);
+    testPizzaOrder.addTopping(testTopping);
+    testPizzaOrder.addTopping(testTopping2);
+    expect(testPizzaOrder.quantity).to.equal(2);
+    expect(testPizzaOrder.pizzaSize).to.equal("small");
+    expect(testPizzaOrder.toppings.length).to.equal(2);
   });
 });
